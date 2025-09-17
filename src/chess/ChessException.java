001f000422cad72abf854bc147674626ea882a3c
Fabiana Aparecida Ribeiro do Vale <fabianavale91@gmail.com>
2025-09-17 12:31:09 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,12 @@
+package chess;
+
+import boardgame.BoardException;
+
+public class ChessException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+		public ChessException(String msg) {
+			super(msg);
+		}
+	 
+}
